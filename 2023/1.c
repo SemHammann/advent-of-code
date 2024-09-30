11c9a71518ba3165727d335953928f347c473d6e
Sem <semhammann@gmail.com>
2024-09-30 12:25:16 +0200

    made deel1 work

--- 2023/1.c
+++ 2023/1.c
@@ -3,31 +3,54 @@
 #include <string.h>
 #include <stdlib.h>
 
-int readt();
+int read();
 int deel1();
 int deel2();
 
 int main (void)
 {
-    deel1(); //put deel1 or deel2 here
+    deel1();
 }
 
 int deel1(void)
 {
-    int r = 0;
-    r = r + readt("treb7uchet");
-    printf ("\n%d", r);
+    int r;
+
+    // Defining file pointer and opening the file.
+    FILE *file_ptr;
+    char str[50];
+
+    // a+ Mode Means that the file is opened in the
+    // append + reading mode
+    file_ptr = fopen("1.txt", "r"); //put here the file name you need to open
+
+    if (NULL == file_ptr) {
+        printf("File can't be opened \n");
+    }
+
+      // Reading stinrg using fgets
+    printf("Deel1\n");
+    while (fgets(str, 50, file_ptr) != NULL) {
+        r = r + read(str);
+        //printf("%d ", read(str));
+        //printf("%d\n", r);
+    }
+
+    fclose(file_ptr);
+    printf ("%d", r);
+    return 0;
 }
 
-int readt(const char *vstring)
+int read(const char *vstring)
 {
     int a = -1, b, i, t;
     char *end;
     for(i = 0; i < strlen(vstring); i++)
     { 
-        t = strtol(&vstring[i], &end, 10);
-        if(t + '0' == vstring[i])
+        t = (int)vstring[i] - '0';       
+        if(t >= 0 && t <= 9)
         {
+            //printf("%d %c ", t, vstring[i]);
             if(a == -1) //if a is already read, just override b, else override both
             {
                 a = t;
@@ -39,5 +62,12 @@ int readt(const char *vstring)
             }
         }
     }
-    return (10*a + b); //return a als the first number and b as the second
+    if(a == -1)
+    {
+        return 0;
+    }
+    else
+    {
+        return (10*a + b); //return a als the first number and b as the second
+    }
 }
