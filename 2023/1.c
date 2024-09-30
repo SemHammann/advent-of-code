0210b9653d46c3d9777918a8b48d684ce9eedf1a
Sem <semhammann@gmail.com>
2024-09-30 15:40:00 +0200

    added the second solution

--- 2023/1.c
+++ 2023/1.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,19 +5,23 @@
 int read();
 int deel1();
 int deel2();
+char *verv();
+char *searchAndReplace();
 
 int main (void)
 {
     deel1();
+    deel2();
 }
 
 int deel1(void)
 {
-    int r;
+    int r1 = 0;
+    int z = 0;
 
     // Defining file pointer and opening the file.
     FILE *file_ptr;
-    char str[50];
+    char str[500];
 
     // a+ Mode Means that the file is opened in the
     // append + reading mode
@@ -30,21 +33,50 @@ int deel1(void)
 
       // Reading stinrg using fgets
     printf("Deel1\n");
-    while (fgets(str, 50, file_ptr) != NULL) {
-        r = r + read(str);
-        //printf("%d ", read(str));
-        //printf("%d\n", r);
+    while (fgets(str, 500, file_ptr) != NULL) {
+        r1 = r1 + read(str);
+        printf("%d\n", z);
+        printf("%s", str);
+        z++;
+        //printf("%d %d %d %s", z, r2, read(str), str;
     }
 
     fclose(file_ptr);
-    printf ("%d", r);
-    return 0;
+    printf ("%d\n", r1);
+}
+
+int deel2(void)
+{
+    int r2 = 0;
+    int z = 0;
+    // Defining file pointer and opening the file.
+    FILE *file_ptr;
+    char str[500], str2[500];
+
+    // a+ Mode Means that the file is opened in the
+    // append + reading mode
+    file_ptr = fopen("1.txt", "r"); //put here the file name you need to open
+
+    rewind(file_ptr);
+    if (NULL == file_ptr) {
+        printf("File can't be opened \n");
+    }
+
+      // Reading stinrg using fgets
+    printf("Deel2\n");
+    while (fgets(str, 500, file_ptr) != NULL) {
+        r2 = r2 + read(verv(str));
+        //z++;
+        //printf("%d %d %d %s", z, r2, read(verv(str)), verv(str));
+    }
+
+    fclose(file_ptr);
+    printf ("%d", r2);
 }
 
 int read(const char *vstring)
 {
     int a = -1, b, i, t;
-    char *end;
     for(i = 0; i < strlen(vstring); i++)
     { 
         t = (int)vstring[i] - '0';       
@@ -70,4 +102,47 @@ int read(const char *vstring)
     {
         return (10*a + b); //return a als the first number and b as the second
     }
+}
+
+char *verv(const char *vstring)
+{
+    char output[500];
+    char *output_p = output;
+
+    strcpy(output, vstring);
+
+    //replace the written numbers with the number
+    output_p = searchAndReplace(output_p, "one", "o1e");
+    output_p = searchAndReplace(output_p, "two", "t2o");
+    output_p = searchAndReplace(output_p, "three", "t3e");
+    output_p = searchAndReplace(output_p, "four", "f4r");
+    output_p = searchAndReplace(output_p, "five", "f5e");
+    output_p = searchAndReplace(output_p, "six", "s6x");
+    output_p = searchAndReplace(output_p, "seven", "s7n");
+    output_p = searchAndReplace(output_p, "eight", "e8t");
+    output_p = searchAndReplace(output_p, "nine", "n9e");
+
+    return output_p;
+}
+
+char *searchAndReplace(char *text, char *search, char *replace)
+{
+   char buffer[1000];
+   char *ptr;
+   char *modText = NULL;
+
+   buffer[0] ='\0';
+   while ( ptr = strstr(text, search) )
+   {
+      strncat(buffer, text, ptr-text);
+      strcat(buffer, replace);
+
+      text = ptr + 1;
+
+   }
+   strcat(buffer, text);
+
+   modText = malloc(strlen(buffer) + 1);
+   strcpy(modText, buffer);
+   return modText;
 }
