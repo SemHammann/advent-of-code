9bb4e3fd399d5f03d17651b9d1e48d7248faa0f3
Sem <sem@ie-design.nl>
2024-10-03 19:14:15 +0200

    started with 2

--- 2023/2.c
+++ 2023/2.c
@@ -4,9 +4,29 @@
 
 int deel1();
 int deel2();
+int possible();
 
 int main(void)
 {
     deel1();
     //deel2();
 }
+
+int deel1(void)
+{
+    char game[] = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    //printf("%s\n", game);
+    possible(game);
+
+}
+
+int deel2(void)
+{
+
+}
+
+int possible(const char *vstring)
+{
+    
+}
+
