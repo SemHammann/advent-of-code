280bcf207167ace598669f0037fc1606b16203c9
Sem <sem@ie-design.nl>
2024-10-16 14:26:31 +0200

    fixed some things

--- libraries/readfile.c
+++ libraries/readfile.c
@@ -2,37 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 char **read(const char *files)
 {
+    // Defining file pointer and opening the file.
     FILE *file_ptr;
-    char str[4096];
+    char str[4096]; // '\0''
     int numlines = 0;
     int maxstrlength = 1;
     int i = 0;
     char ch;
-
+    
     file_ptr = fopen(files, "r");
 
-    /*while((ch = fgets(str, 4095, file_ptr) != NULL))
+    while((ch = fgets(str, 4095, file_ptr) != NULL))
     {
         numlines++;
-        if (strlen(str) > maxstrlength)
-        {
-            //maxstrlength = sizeof(str); //deze klopt nog niet
-        }
-    }*/
-
+    }
     rewind(file_ptr);
-
     const int size = numlines*sizeof(char*);
+    printf("Size of the array is %d x %d\n", numlines, maxstrlength);
     char **output = malloc(size);
     for (i = 0; i < numlines; i++)
     {
         output[i] = (char*)malloc(4096 * sizeof(char));
         *output[i] = 0;
     }
-
     if (NULL == file_ptr) {
         printf("File can't be opened \n");
     }
@@ -41,12 +35,10 @@ char **read(const char *files)
         printf("File open worked\n");
         i = 0;
     }
-
     while (fgets(str, 4095, file_ptr) != NULL) {
         strcpy(output[i], str);
         i++;
     }
-
     fclose(file_ptr);
     return output;
 }
