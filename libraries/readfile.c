375e4a8c7f43e38d46d2adb1198bc8288d7ad064
Sem <sem@ie-design.nl>
2024-10-19 19:48:49 +0200

    changed the files, so they use a constuct

--- libraries/readfile.c
+++ libraries/readfile.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "readfile.h"
 
-char **read(const char *files)
+struct filecontent read(const char *files)
 {
     FILE *file_ptr;
     char str[4096];
@@ -11,18 +11,19 @@ char **read(const char *files)
     int maxstrlength = 1;
     size_t i = 0;
     char ch;
-    
+    struct filecontent read;
+
     file_ptr = fopen(files, "r");
 
     while((ch = fgets(str, 4095, file_ptr) != NULL))
     {
-        numlines++;
+        read.lengthfile++;
     }
     rewind(file_ptr);
-    const size_t size = numlines*sizeof(char*);
-    printf("Size of the array is %llu x %d\n", numlines, maxstrlength);
+    const size_t size = read.lengthfile*sizeof(char*);
+    //printf("Size of the array is %llu x %d\n", numlines, maxstrlength);
     char **output = malloc(size);
-    for (i = 0; i < numlines; i++)
+    for (i = 0; i < read.lengthfile; i++)
     {
         output[i] = (char*)malloc(4096 * sizeof(char));
         *output[i] = 0;
@@ -40,34 +41,6 @@ char **read(const char *files)
         i++;
     }
     fclose(file_ptr);
-    return output;
+    read.file = output;
+    return read;
 }
-
-int lenfile(const char *files)
-{
-    FILE *file_ptr;
-    int numlines = 0;
-    char str[4096];
-    char ch;
-    
-    file_ptr = fopen(files, "r");
-
-    while((ch = fgets(str, 4095, file_ptr) != NULL))
-    {
-        numlines++;
-    }
-    fclose(file_ptr);
-    return numlines;
-}
-
-/*int main(void)
-{
-    int j;
-    const char *filename = "1.txt";
-    char **lines = read(filename);
-    for(j = 0; j < 100; j++)
-    {
-        printf("%s", lines[j]);
-    }
-    printf("%d", lenfile(filename));
-}*/
--- libraries/readfile.h
+++ libraries/readfile.h
@@ -1,6 +1,12 @@
 #ifndef __MYCODE_H__
 #define __MYCODE_H__
 
+struct filecontent
+{
+    char **file;
+    size_t lengthfile;
+};
+
 extern char **read(const char *filename);
 extern int lenfile(const char *filename);
 
