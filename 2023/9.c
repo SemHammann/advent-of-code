934bb9c667bae763d36dc30fb5723b30838e6e9d
Sem <sem@ie-design.nl>
2024-11-13 16:42:28 +0100

    the testfile works, the main not yet

---
+++ 2023/9.c
@@ -0,0 +1,141 @@
+//V3.2
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include "..\libraries\adventofcode.c"
+
+//too low 1551709655
+//too low 1533864819
+
+//#define TEST1
+//#define TEST2
+
+#ifdef TEST1
+    #define filename "txt/9.test1.txt"
+#elif defined TEST2
+	#define filename "txt/9.txt1.txt"
+#else
+    #define filename "txt/9.txt"
+#endif
+
+#define stringlength 4096
+#define arraylength 128
+
+void part1();
+void part2();
+
+int difference();
+int str_to_int();
+void fill_int_min();
+
+int main(void)
+{
+	clock_t begin = clock();
+	file = readfile(filename);
+
+	
+	clock_t begin1 = clock();
+	printf("\n%s\nThings for 1 and 2: %lfms\n\n", filename, (double)(begin1 - begin));
+	part1();
+	clock_t end1 = clock();
+	printf("\n%lfms\n\n", (double)(end1 - begin1));
+	part2();
+	clock_t end2 = clock();
+	printf("\n%lfms", (double)(end2 - end1));
+}
+
+void part1()
+{
+	int answer = 0;
+	char game[stringlength];
+	char **tokens;
+	int numbers[arraylength];
+	size_t j;
+
+	for(size_t i = 0; i < file.lengthfile; i++)
+	{
+		j = 0;
+		fill_int_min(numbers);
+		strcpy(game, file.file[i]);
+		tokens = str_split(game, ' ', true);
+		while(*(tokens + j) != NULL)
+		{
+			numbers[j] = str_to_int(*(tokens + j));
+			j++;
+		}
+		answer = answer + difference(numbers);
+	}
+
+
+	printf("Part 1: %llu", answer);
+}
+
+void part2()
+{
+	int answer;
+
+	printf("Part 2: %llu", answer);
+}
+
+
+int difference(int vstring[])
+{
+	int diff[arraylength];
+	fill_int_min(diff);
+	int i = 0;
+	bool all_zero = true;
+	for(int j = 0; j < arraylength; j++)
+	{
+		if(vstring[j] == INT_MIN)
+		{
+			break;
+		}
+		if(vstring[j] != 0)
+		{
+			all_zero = false;
+			break;
+		}
+	}
+	if(all_zero == true)
+	{
+		return 0;
+	}
+	while(vstring[i + 1] != INT_MIN)
+	{
+		diff[i] = vstring[i + 1] - vstring[i];
+		i++;
+	}
+	return vstring[i] + difference(diff);
+}
+
+int str_to_int(char *vstring)
+{
+	int i = 0, number  = 0;
+	char game[4096];
+	strcpy(game, vstring);
+	while(true)
+	{
+		if(game[i] >= '0' && game[i] <= '9')
+			{
+				number = 10 * number + (game[i] - '0');
+			}
+			else
+			{
+				return number;
+			}   
+		i++;
+	}
+}
+
+void fill_int_min(int *vstring)
+{
+	for(size_t i = 0; i < arraylength; i++)
+	{
+		vstring[i] = INT_MIN;
+	}
+}
