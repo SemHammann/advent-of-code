fd9f5147a0c07194598ca827629469f2622e401a
Sem <sem@ie-design.nl>
2024-11-28 18:04:22 +0100

    deleted some commented code

--- libraries/queue.h
+++ libraries/queue.h
@@ -1,16 +1,10 @@
 //V4.2
 
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef QUEUE_BLOEM_H
+#define QUEUE_BLOEM_H
 
 #include <stdbool.h>
 
-/*#ifndef QUEUELENGTH
-	#define QUEUELENGTH 128
-#endif
-#ifndef QUEUEHEIGHT
-	#define QUEUEHEIGHT 128	
-#endif*/
 
 typedef struct Char_Queue
 {
@@ -39,7 +33,7 @@ typedef struct long_long_Queue
 }long_long_Queue_t;
 //struct long_long_Queue long_long_queue;
 
-void init_c(struct Char_Queue *char_queue);
+void init_c(struct Char_Queue *char_queue, size_t length, size_t height);
 void push_front_c(char vstr[], struct Char_Queue *char_queue);
 void push_back_c(char vstr[], struct Char_Queue *char_queue);
 char *pop_front_c(struct Char_Queue *char_queue);
@@ -47,8 +41,7 @@ char *pop_back_c(struct Char_Queue *char_queue);
 char *front_c(struct Char_Queue *char_queue);
 char *back_c(struct Char_Queue *char_queue);
 
-
-void init_llu(struct long_long_unsigned_Queue *long_long_unsigned_queue);
+void init_llu(struct long_long_unsigned_Queue *long_long_unsigned_queue, size_t length);
 void push_front_llu(long long unsigned number, struct long_long_unsigned_Queue *long_long_unsigned_queue);
 void push_back_llu(long long unsigned number, struct long_long_unsigned_Queue *long_long_unsigned_queue);
 long long unsigned pop_front_llu(struct long_long_unsigned_Queue *long_long_unsigned_queue);
@@ -64,7 +57,4 @@ long long pop_back_ll(struct long_long_Queue *long_long_queue);
 long long front_ll(struct long_long_Queue *long_long_queue);
 long long back_ll(struct long_long_Queue *long_long_queue);
 
-
-
-
-#endif /* QUEUE_H */
+#endif /* QUEUE_BLOEM_H */
