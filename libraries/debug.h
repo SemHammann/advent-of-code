bd6b2bb2f5de9b68738fbb79792db3afdcb881c0
Sem <sem@ie-design.nl>
2024-11-28 13:36:33 +0100

    added a warning if DEBUG or _DEBUG is not defined

--- libraries/debug.h
+++ libraries/debug.h
@@ -3,6 +3,10 @@
 #ifndef DEBUG_SEM_H
 #define DEBUG_SEM_H
 
+#if !defined(DEBUG) && !defined(_DEBUG)
+	#warning "DEBUG or _DEBUG not defined"	
+#endif
+
 int debug_print(const char *str, ...);
 
 #endif // DEBUG_SEM_H
