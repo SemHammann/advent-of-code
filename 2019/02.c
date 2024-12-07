4c2ed8673c97e5912f7b7333f4c256a52d42e8df
Sem <sem@ie-design.nl>
2024-12-07 12:05:31 +0100

    part 1 works

---
+++ 2019/02.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <limits.h>
+#include <inttypes.h>
+
+#include "../libraries/adventofcode.c" //V5.1
+#include "../libraries/queue.c" //V4.2
+#define _DEBUG
+#include "../libraries/debug.c" //V0.1
+
+
+struct IntCode
+{
+	long long *reg;
+	size_t current;
+};
+struct IntCode IC;
+
+
+void part1();
+void part2();
+
+void setup();
+void ICadd();
+void ICmul();
+
+int main(int argc, char *argv[])
+{
+	clock_t begin = clock();
+	
+	fix_file(argv, "M");
+
+	
+
+	clock_t begin1 = clock();
+	printf("\nThings for 1 and 2: %.0lf ms\n\n", (double)(begin1 - begin)/CLOCKS_PER_SEC*1000);
+	part1();
+	clock_t end1 = clock();
+	printf("\n%.0lf ms\n\n", (double)(end1 - begin1)/CLOCKS_PER_SEC*1000);
+	part2();
+	clock_t end2 = clock();
+	printf("\n%.0lf ms", (double)(end2 - end1)/CLOCKS_PER_SEC*1000);
+	return 0;
+}
+
+void part1() 
+{
+	bool end = false;
+	setup();
+
+	*(IC.reg + 1) = 12;
+	*(IC.reg + 2) = 2;
+
+	while(end == false)
+	{
+		switch(*(IC.reg + IC.current))
+		{
+			case 1:
+				ICadd();
+				break;
+			case 2:
+				ICmul();
+				break;
+			case 99:
+				end = true;
+				break;
+			default:
+				IC.current++;
+				break;
+		}
+	}
+	printf("Part 1: %lld", *(IC.reg));
+	free(IC.reg);
+}
+
+void part2()
+{
+	long long answer = 0;
+
+	setup();
+
+	printf("Part 2: %lld", answer);
+}
+
+void setup()
+{
+	char *game;
+	char **tokens;
+	size_t i = 0;
+	game = calloc(file.lengthlines[0], sizeof(char));
+	strcpy(game, file.file[0]);
+	IC.current = 0;
+
+	tokens = str_split(game, ',', false);
+	while(*(tokens + i) != NULL)
+	{
+		i++;
+	}
+	IC.reg = calloc(i, sizeof(long long));
+
+	for(size_t j = 0; j < i; j++)
+	{
+		*(IC.reg + j) = str_ll(*(tokens + j));
+	}
+	return;
+}
+
+void ICadd()
+{
+	*(IC.reg + *(IC.reg + IC.current + 3)) = *(IC.reg + *(IC.reg + IC.current + 1)) + *(IC.reg + *(IC.reg + IC.current + 2));
+
+	IC.current += 4;
+	return;
+}
+
+void ICmul()
+{
+	*(IC.reg + *(IC.reg + IC.current + 3)) = *(IC.reg + *(IC.reg + IC.current + 1)) * *(IC.reg + *(IC.reg + IC.current + 2));
+
+	IC.current += 4;
+	return;
+}
