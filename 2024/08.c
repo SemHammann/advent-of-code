52d47a6c23b9cda67549e533ba10e4a9a649c76c
Sem <sem@ie-design.nl>
2024-12-10 15:37:02 +0100

    updated with new libraries

--- 2024/08.c
+++ 2024/08.c
@@ -1,48 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <time.h>
-#include <math.h>
-#include <limits.h>
-#include <inttypes.h>
-
 #include "../libraries/adventofcode.c" //V5.1
-#include "../libraries/queue.c" //V4.2
-#define _DEBUG
-#include "../libraries/debug.c" //V0.1
-
 
 char **antinodes;
 
-void part1();
-void part2();
-
-void fill_antinodes(char frequency);
-void fill_antinodes2(char frequency);
+void fill_antinodes(char frequency, int part);
 long long count_antinodes();
 
 int main(int argc, char *argv[])
 {
 	clock_t begin = clock();
-	
 	fix_file(argv, "M");
 	antinodes = calloc(file.amountlines + 1, sizeof(char*));
 	for(size_t i = 0; i < file.amountlines; i++)
-	{
 		*(antinodes + i) = calloc(file.lengthlines[i] + 1, sizeof(char));
-	}
-
-
-	clock_t begin1 = clock();
-	printf("\nThings for 1 and 2: %.0lfms\n\n", (double)(begin1 - begin)/CLOCKS_PER_SEC*1000);
-	part1();
-	clock_t end1 = clock();
-	printf("\n%.0lfms\n\n", (double)(end1 - begin1)/CLOCKS_PER_SEC*1000);
-	part2();
-	clock_t end2 = clock();
-	printf("\n%.0lfms", (double)(end2 - end1)/CLOCKS_PER_SEC*1000);
+	run_parts(begin);
 }
 
 void part1() 
@@ -50,11 +20,8 @@ void part1()
 	long long answer = 0;
 
 	for(char i = 0; i < 127; i++)
-	{
 		if(i != '.')
-			fill_antinodes(i);
-	}
-
+			fill_antinodes(i, 1);
 	answer = count_antinodes();
 	printf("Part 1: %lld", answer);
 }
@@ -64,73 +31,36 @@ void part2()
 	long long answer = 0;
 
 	for(char i = 0; i < 127; i++)
-	{
 		if(i != '.')
-			fill_antinodes2(i);
-	}
-
+			fill_antinodes(i, 2);
 	answer = count_antinodes();
-
 	printf("Part 2: %lld", answer);
 }
 
-void fill_antinodes(char frequency)
-{
-	long long yt, xt;
-	for(long long y1 = 0; y1 < file.amountlines; y1++)
-	{
-		for(long long x1 = 0; x1 < file.lengthlines[y1]; x1++)
-		{
-			if(file.file[y1][x1] == frequency)
-			{
-			for(long long y2 = 0; y2 < file.amountlines; y2++)
-			{
-				for(long long x2 = 0; x2 < file.lengthlines[y2]; x2++)
-				{
-					if(!(x1 == x2 && y1 == y2) && file.file[y2][x2] == frequency)
-					{
-						yt = y1 - y2;
-						xt = x1 - x2;
-						if(x1 + xt >= 0 && x1 + xt < file.lengthlines[0] && y1 + yt >= 0 && y1 + yt < file.amountlines)
-							antinodes[y1 + yt][x1 + xt] = '#';
-					}
-				}
-			}
-			}
-		}
-	}
-}
-
-void fill_antinodes2(char frequency)
+void fill_antinodes(char frequency, int part)
 {
 	long long yt, xt;
 	long long count;
 	for(long long y1 = 0; y1 < file.amountlines; y1++)
-	{
 		for(long long x1 = 0; x1 < file.lengthlines[y1]; x1++)
-		{
 			if(file.file[y1][x1] == frequency)
-			{
-			for(long long y2 = 0; y2 < file.amountlines; y2++)
-			{
-				for(long long x2 = 0; x2 < file.lengthlines[y2]; x2++)
-				{
-					if(!(x1 == x2 && y1 == y2) && file.file[y2][x2] == frequency)
-					{
-						yt = y1 - y2;
-						xt = x1 - x2;
-						count = 0;
-						while(x1 + xt * count >= 0 && x1 + xt * count < file.lengthlines[0] && y1 + yt * count >= 0 && y1 + yt * count < file.amountlines)
+				for(long long y2 = 0; y2 < file.amountlines; y2++)
+					for(long long x2 = 0; x2 < file.lengthlines[y2]; x2++)
+						if(!(x1 == x2 && y1 == y2) && file.file[y2][x2] == frequency)
 						{
-							antinodes[y1 + yt * count][x1 + xt * count] = '#';
-							count++;
+							yt = y1 - y2;
+							xt = x1 - x2;
+							count = 0;
+							if(part == 1)
+								if(x1 + xt >= 0 && x1 + xt < file.lengthlines[0] && y1 + yt >= 0 && y1 + yt < file.amountlines)
+									antinodes[y1 + yt][x1 + xt] = '#';
+							if(part == 2)
+								while(x1 + xt * count >= 0 && x1 + xt * count < file.lengthlines[0] && y1 + yt * count >= 0 && y1 + yt * count < file.amountlines)
+								{
+									antinodes[y1 + yt * count][x1 + xt * count] = '#';
+									count++;
+								}
 						}
-					}
-				}
-			}
-			}
-		}
-	}
 }
 
 long long count_antinodes()
@@ -140,6 +70,5 @@ long long count_antinodes()
 		for(size_t x = 0; x < file.lengthlines[y]; x++)
 			if(antinodes[y][x] == '#')
 				answer++;
-
 	return answer;
 }
