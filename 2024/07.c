9198c78d5b1826201e12f4908d5803b98af1334a
Sem <sem@ie-design.nl>
2024-12-10 15:31:35 +0100

    updated with new libraries

--- 2024/07.c
+++ 2024/07.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <time.h>
-#include <math.h>
-#include <limits.h>
-#include <inttypes.h>
-
 #include "../libraries/adventofcode.c" //V5.1
-#include "../libraries/queue.c" //V4.2
-#define _DEBUG
-#include "../libraries/debug.c" //V0.1
 
 struct Data
 {
@@ -21,49 +8,30 @@ struct Data
 };
 struct Data data;
 
-
-void part1();
-void part2();
-
 void fill_data();
 long long possible();
-long long possible2();
 
 int main(int argc, char *argv[])
 {
 	clock_t begin = clock();
-	
 	fix_file(argv, "M");
 	fill_data();
-
-
-	clock_t begin1 = clock();
-	printf("\nThings for 1 and 2: %.0lf ms\n\n", (double)(begin1 - begin)/CLOCKS_PER_SEC*1000);
-	part1();
-	clock_t end1 = clock();
-	printf("\n%.0lf ms\n\n", (double)(end1 - begin1)/CLOCKS_PER_SEC*1000);
-	part2();
-	clock_t end2 = clock();
-	printf("\n%.0lf ms", (double)(end2 - end1)/CLOCKS_PER_SEC*1000);
+	run_parts(begin);
 }
 
 void part1() 
 {
 	long long answer = 0;
-
 	for(size_t i = 0; i < file.amountlines; i++)
-		answer += possible(i, 0, 0);
-
+		answer += possible(i, 0, 0, 1);
 	printf("Part 1: %lld", answer);
 }
 
 void part2()
 {
 	long long answer = 0;
-
 	for(size_t i = 0; i < file.amountlines; i++)
-		answer += possible2(i, 0, 0);
-
+		answer += possible(i, 0, 0, 2);
 	printf("Part 2: %lld", answer);
 }
 
@@ -98,28 +66,7 @@ void fill_data()
 	}
 }
 
-long long possible(size_t place, size_t where, size_t current)
-{
-	long long mul, add;
-	if(where >= *(data.amount_input + place))
-	{
-		if(current == *(data.answers + place))
-			return *(data.answers + place);
-		else
-			return 0;
-	}
-	add = current + *(*(data.input + place) + where);
-	if(current == 0 && where == 0)
-		current = 1;
-	mul = current * *(*(data.input + place) + where);
-	
-	if(possible(place, where + 1, mul) == *(data.answers + place) || possible(place, where + 1, add) == *(data.answers + place))
-		return *(data.answers + place);
-	else
-		return 0;
-}
-
-long long possible2(size_t place, size_t where, size_t current)
+long long possible(size_t place, size_t where, size_t current, int part)
 {
 	long long mul, add, cons, fact;
 	if(where >= *(data.amount_input + place))
@@ -137,8 +84,11 @@ long long possible2(size_t place, size_t where, size_t current)
 	mul = current * *(*(data.input + place) + where);
 	
 		
-	if(possible2(place, where + 1, mul) == *(data.answers + place) || possible2(place, where + 1, add) == *(data.answers + place) || possible2(place, where + 1, cons) == *(data.answers + place))
+	if(possible(place, where + 1, mul, part) == *(data.answers + place) || possible(place, where + 1, add, part) == *(data.answers + place))
 		return *(data.answers + place);
-	else
-		return 0;
+	if(part == 2)
+		if(possible(place, where + 1, cons, part) == *(data.answers + place))
+			return *(data.answers + place);
+	
+	return 0;
 }
